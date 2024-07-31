6870f01baf2c0ea130c03246bc80b10e0b5e8b1e
Vikas <81951052+vkas9@users.noreply.github.com>
2024-08-01 00:42:08 +0530

    use

--- src/app/[[...rest]]/page.tsx
+++ src/app/[[...rest]]/page.tsx
@@ -6,11 +6,14 @@ import {
   useAuth,
   UserButton,
   UserProfile,
+  useUser,
 } from "@clerk/nextjs";
 import { currentUser } from "@clerk/nextjs/server";
 import Link from "next/link";
 
 export default async function Home() {
+  const userr=useUser();
+  console.log("userrr=->",userr)
 const user= await  currentUser();
 console.log("user",user)
 
