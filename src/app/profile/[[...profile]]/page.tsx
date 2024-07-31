f3da4e2e7e4b0b0ccbe949d793b1ad072950ca10
Vikas <81951052+vkas9@users.noreply.github.com>
2024-08-01 01:14:13 +0530

    sd

--- src/app/profile/[[...profile]]/page.tsx
+++ src/app/profile/[[...profile]]/page.tsx
@@ -1,17 +1,13 @@
 "use client"
 import { UserProfile, useUser } from "@clerk/nextjs"
-import { auth, currentUser } from "@clerk/nextjs/server";
-import { redirect } from "next/navigation";
 
-export default async function Profile(){
+
+export default function Profile(){
 
   
  const user=useUser()
  console.log("user-s",user)
    
-    // if (!user?.isLoaded || !user?.isSignedIn) {
-    //     return <h1>Hello nothing</h1>;
-    //   }
     
       return <div className=" flex items-center justify-center font-bold text-3xl h-screen"><UserProfile/></div>;
 
