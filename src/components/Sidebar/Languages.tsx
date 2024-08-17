871a03e48beef5ea91d00231ac45d95fc4202c28
Vikas <81951052+vkas9@users.noreply.github.com>
2024-08-17 21:18:34 +0530

    skell handled

--- src/components/Sidebar/Languages.tsx
+++ src/components/Sidebar/Languages.tsx
@@ -1,35 +1,43 @@
-"use client"
-import { languages } from '@/constants/languages'
-import { languagesAction } from '@/lib/store/features/languagesSlice'
-import { useAppDispatch, useAppSelector } from '@/lib/store/hooks'
-import Link from 'next/link'
-import React from 'react'
+"use client";
+import { languages } from "@/constants/languages";
+import { languagesAction } from "@/lib/store/features/languagesSlice";
+import { useAppDispatch, useAppSelector } from "@/lib/store/hooks";
+import Link from "next/link";
+import React from "react";
+import { Skeleton } from "../ui/skeleton";
 
 const Languages = () => {
-
-  const {LanguageCounter}=useAppSelector(state=>state.language)
-  const dispatch=useAppDispatch()
-  const handleLink=(_id:string)=>{
-      dispatch(languagesAction.setLanguages(_id))
-  }
+  const { LanguageCounter } = useAppSelector((state) => state.language);
+  const { loadingSnippets } = useAppSelector((state) => state.quicklink);
+  const dispatch = useAppDispatch();
+  const handleLink = (_id: string) => {
+    dispatch(languagesAction.setLanguages(_id));
+  };
   return (
     <>
-     {
-              LanguageCounter?.map((item,index)=>{
-                return <Link key={item._id} href={"/snippets/all-snippets"}>
-                  <div  className={`flex items-center gap-1 hover:bg-white/10 ${item.isSelected?"bg-white/10":""} justify-between z p-2 rounded-md`}>
-                                <div className='flex items-center gap-2 '>
-
-                                <item.icon/>                                
-                                <span>{item.title}</span>
-                                </div>
-                                <span className='text-white/30 text-sm'>{item?.count}</span>
-                            </div>
-                </Link>
-              })
-            }
+      {loadingSnippets
+        ? Array.from({ length: 5 }).map((_, index) => (
+            <Skeleton key={index} className="min-h-[40px] min-w-[172px] " />
+          ))
+        : LanguageCounter?.map((item, index) => {
+            return (
+              <Link key={item._id} href={"/snippets/all-snippets"}>
+                <div
+                  className={`flex items-center gap-1 hover:bg-white/10 ${
+                    item.isSelected ? "bg-white/10" : ""
+                  } justify-between z p-2 rounded-md`}
+                >
+                  <div className="flex items-center gap-2 ">
+                    <item.icon />
+                    <span>{item.title}</span>
+                  </div>
+                  <span className="text-white/30 text-sm">{item?.count}</span>
+                </div>
+              </Link>
+            );
+          })}
     </>
-  )
-}
+  );
+};
 
-export default Languages
+export default Languages;
