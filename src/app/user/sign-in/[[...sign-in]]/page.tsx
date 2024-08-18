52d349d126ba07b54e8f11431ea6dc3b761d202d
Vikas <81951052+vkas9@users.noreply.github.com>
2024-08-18 15:31:37 +0530

    redirecafter signedin

--- src/app/user/sign-in/[[...sign-in]]/page.tsx
+++ src/app/user/sign-in/[[...sign-in]]/page.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Signin = () => {
   return (
     <div className='mx-auto mt-6'>
-      <SignIn/>
+      <SignIn forceRedirectUrl={"snippets/all-snippets"}/>
     </div>
   )
 }
