e91e638bbe6b9ece6a9ca10381a9727502c4653e
Vikas <81951052+vkas9@users.noreply.github.com>
2024-08-19 15:05:04 +0530

    dfg

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,7 +1,7 @@
-"use client"
-import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+"use client";
+import { SignedIn, SignedOut, useAuth, UserButton } from "@clerk/nextjs";
 import Link from "next/link";
-import React from "react";
+import React, { useEffect } from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { FaArrowRight } from "react-icons/fa6";
 import SearchBar from "../searchBar/searchBar";
@@ -16,82 +16,78 @@ import UserAuth from "./UserAuth";
 import { DrawerDemo } from "./Popup";
 import SnippetModal from "../content/SnippetOpen/SnippetModal";
 
-
 const Navbar = () => {
+  const {isSignedIn,isLoaded}=useAuth()
   const dispatch = useAppDispatch();
-  const {isMobileView,items}= useAppSelector(state=>state.quicklink)
+  const { isMobileView, items } = useAppSelector((state) => state.quicklink);
   const router = useRouter();
   const handleLink = async (id: string, link: string) => {
     await dispatch(quickLinkAction.setQuickLink(id));
     router.push(`/snippets/${link}`);
-};
+  };
 
-const handleMouseEnter = (link: string) => {
-   
+  const handleMouseEnter = (link: string) => {
     router.prefetch(`/snippets/${link}`);
-};
+  };
+  
+
+  if (!isLoaded) return null;
   return (
     <>
-    
+      <SnippetModal />
+      <div
+        className={`  w-full   flex items-center h-[45px] sm:h-[60px] ${!isSignedIn?"pl-2":""} justify-between  py-2 sm:pl-6 pr-4 ${
+          items[2].isSelected ? "bg-red-500/10" : "bg-[#030836]"
+        } bg-[#030836] mx-auto`}
+      >
+        {/* Home */}
 
-    <SnippetModal/>
-    <div className={`  w-full   flex items-center h-[45px] sm:h-[60px] justify-between py-2 sm:pl-6 pr-4 ${
-      items[2].isSelected ? "bg-red-500/10" : "bg-[#030836]"
-    } bg-[#030836] mx-auto`}>
-      {/* Home */}
-      
-      <div className=" flex items-center gap-2 ">
+        <div className=" flex items-center gap-2 ">
+          <SignedIn>
+            <OpenCloseSidebar isMobileView={isMobileView} />
+          </SignedIn>
+          <Link href={"/"} className="relative">
+            <div className="flex   text-lg sm:text-2xl items-center">
+              <span className="font-bold ">Snippet</span>
+              <span className="font-thin">Hub</span>
+            </div>
+
+            <BackgroundBeams />
+          </Link>
+        </div>
         <SignedIn>
-
-       
- <OpenCloseSidebar isMobileView={isMobileView}/>
-      </SignedIn>
-        <Link href={"/"} className="relative">
-          <div className="flex   text-lg sm:text-2xl items-center">
-            <span className="font-bold ">Snippet</span>
-            <span className="font-thin">Hub</span>
+          <SearchBar />
+        </SignedIn>
+        {/*User Auth */}
+        <SignedOut>
+          <div className="sm:hidden">
+            <DrawerDemo isMobileView={isMobileView} />
           </div>
+        </SignedOut>
 
-          <BackgroundBeams />
-        </Link>
-      </div>
-      <SignedIn>
-        <SearchBar />
-      </SignedIn>
-      {/*User Auth */}
-      <SignedOut>
-        <div className="sm:hidden">
-        <DrawerDemo isMobileView={isMobileView} />
-        </div>
-      
-      </SignedOut>
-
-      <SignedIn>
-        <div className=" flex  gap-2 sm:gap-4 items-center ">
-          <div   
-            onClick={() => handleLink("1", "all-snippets")}
-            onMouseEnter={() => handleMouseEnter("all-snippets")}
-            className="bg-white/10 max-sm:hidden hover:cursor-pointer  flex items-center group gap-3 active:bg-white/20 sm:hover:bg-white/20 transition-all duration-100 py-1 sm:py-2 px-4 sm:px-6 rounded-md font-bold text-md "
-          >
-            <span>My Snippets</span>
-            {/* <FaArrowRight className='hidden transition-all duration-200 group-hover:flex'/> */}
+        <SignedIn>
+          <div className=" flex  gap-2 sm:gap-4 items-center ">
+            <div
+              onClick={() => handleLink("1", "all-snippets")}
+              onMouseEnter={() => handleMouseEnter("all-snippets")}
+              className="bg-white/10 max-sm:hidden hover:cursor-pointer  flex items-center group gap-3 active:bg-white/20 sm:hover:bg-white/20 transition-all duration-100 py-1 sm:py-2 px-4 sm:px-6 rounded-md font-bold text-md "
+            >
+              <span>My Snippets</span>
+              {/* <FaArrowRight className='hidden transition-all duration-200 group-hover:flex'/> */}
+            </div>
+            <div className="h-[40px]  max-sm:hidden  sm:h-[50px] rounded-full min-w-[1px] bg-white/20 "></div>
+            <UserButton />
           </div>
-          <div className="h-[40px]  max-sm:hidden  sm:h-[50px] rounded-full min-w-[1px] bg-white/20 "></div>
-          <UserButton />
-        </div>
-      </SignedIn>
+        </SignedIn>
 
-      <SignedOut>
-        <div className="hidden sm:block">
-
-       
-      <UserAuth/>
+        <SignedOut>
+          <div className="hidden sm:block">
+            <UserAuth />
+          </div>
+        </SignedOut>
       </div>
-      </SignedOut>
-    </div>
     </>
   );
-  
 };
 
 export default Navbar;
