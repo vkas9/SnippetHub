0bf19003c8a1bae9d4d7ef5a80442f3d77dc57d6
Vikas <81951052+vkas9@users.noreply.github.com>
2024-08-19 15:03:24 +0530

    sdfsdf

--- src/actions/user.action.ts
+++ src/actions/user.action.ts
@@ -5,6 +5,10 @@ export default async function createUser(user:any){
     try {
         await connectDB();
         console.log(global)
+        const isUserExist=await User.findOne({clerkId:user?.clerkId});
+        if(isUserExist){
+          return "user Already exist"
+        }
         const newUser = await User.create(user);
         console.log("newUser",newUser)
         return JSON.parse(JSON.stringify(newUser));
